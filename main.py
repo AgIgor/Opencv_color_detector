b7683160d408b5889242fc911590c4019dd1b09d
AgIgor <agigor@outlook.com>
2023-01-25 22:36:33 -0300

    sync colors

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 
 # cap = cv2.VideoCapture('collors.png')
 cap = cv2.VideoCapture('hd.mp4')
 # cap = cv2.VideoCapture(0)
 
-start_point = (101, 101)
-end_point = (529, 249)
-color = (0, 0, 0)
-thickness = -1
-
 if not cap.isOpened():
     print("Error opening video stream or file")
 
 while cap.isOpened():
-    fps = int(cap.get(5))
-    frame_count = cap.get(7)
+    # fps = int(cap.get(5))
+    # frame_count = cap.get(7)
     # print("Frame Rate : ", fps, "frames per second"," Frame count : ", frame_count)
-
     ret, frame = cap.read()
+    frame = cv2.resize(frame, (1280, 720))
     h, w = frame.shape[:2]
-    # print(h, w)
+    # print(f'h = {h}, w = {w}')
 
     if ret:
-        # frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
+        # time.sleep(.07)
+        # leds_side = 35
 
-        # frame = cv2.rectangle(frame, (0, 0), (100, 100), (0, 255, 0), -1)
-        # frame = cv2.rectangle(frame, (0, 250), (10, 10), (0, 255, 0), -1)
+        led_pos_inicio = 25
+        for i in range(0, 700, 50):
+
+            (b, g, r) = (frame[led_pos_inicio + 10 + i, led_pos_inicio])# # \/ , >
+
+            frame = cv2.rectangle(frame, (10 - 1, led_pos_inicio + i - 1), (20 + 1, led_pos_inicio + 10 + i + 1), (0, 0, 0), -1)# >,\/    >, \/
+            frame = cv2.rectangle(frame, (10, led_pos_inicio + i), (20, led_pos_inicio + 10 + i), (int(b), int(g), int(r)), -1)# >,\/    >, \/
 
         cv2.imshow('Frame', frame)
-        # (b, g, r) = frame[0, 0]
-        # print(h, w, r, g, b)
 
-        for p in range(0, h, 100):
-            (b, g, r) = (frame[0, p])
-            print(f'{h},{w} [{r},{g},{b}]')
-
-        # Press Q on keyboard to  exit
+        # Press Q on keyboard to  exit* i
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
